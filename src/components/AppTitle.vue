0ccfe2cad688c5e37d600f92c1371bccd1968606
Brandon Vasquez <thatsjaskett@gmail.com>
2023-02-03 16:36:31 +0100

    Esercizio completato

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: "AppTitle",
+    data() {
+        return {
+            msg: "La mia prima app con Vite"
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+</template>
+
+<style scoped>
+h1 {
+    color: red;
+    font-style: italic;
+}
+</style>
